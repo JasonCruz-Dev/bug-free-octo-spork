4dcf5b544ee419abcf49f0c8c2ef44fa3ef36a6a
Jason Cruz <badmonkey.dev@gmail.com>
2020-03-23 18:15:35 -0500

    fixed routes

--- src/App.js
+++ src/App.js
@@ -3,39 +3,32 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 
-import Projects from "./C/Projects";
-import About from "./C/About";
-import Contact from "./C/Contact";
 import NavBar from "./C/NavBar";
+import About from "./C/About";
+import Projects from "./C/Projects";
+import Contact from "./C/Contact";
 
 function App() {
   return (
     <div className='App'>
       <header id='header'>
-        <div class='logo'>
-          <span class='icon fa-gem'></span>
+        <div className='logo'>
+          <span className='icon fa-gem'></span>
         </div>
-        <div class='content'>
-          <div class='inner'>
+        <div className='content'>
+          <div className='inner'>
             <h1>Jason Cruz</h1>
             <p>Full Stack Engineer - Iot Enthusiast - Polyglot</p>
           </div>
         </div>
         <nav>
-          <ul>
-            <NavBar>
-              <li>
-                <Route path='/project' component={Projects} />
-              </li>
-              <li>
-                <Route path='/about' component={About} />
-              </li>
-              <li>
-                <Route path='/contact' component={Contact} />
-              </li>
-            </NavBar>
-          </ul>
+          <NavBar />
         </nav>
+        <div>
+          <Route path='/projects' component={Projects} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </div>
       </header>
     </div>
   );
--- src/C/NavBar.js
+++ src/C/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+// import { Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+
+// import About from "./About";
 
 function NavBar() {
   return (
